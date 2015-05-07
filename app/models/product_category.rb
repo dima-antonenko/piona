23c01ffecf09822ef64f5a79da2a6186913290b2
dima-antonenko <d.antonenko94@mail.ru>
2015-05-08 02:15:15 +0300

    add  view to product_category

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -3,7 +3,7 @@ class ProductCategory < ActiveRecord::Base
 	belongs_to :product_category
 	has_many :products, dependent: :destroy
 
-	has_attached_file :avatar, :styles => { :medium => "340x280>", :thumb => "137x137>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "303x244>", :thumb => "220x180" }, :default_url => "/images/:style/missing.png"
 
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
     validates :name, presence: true
