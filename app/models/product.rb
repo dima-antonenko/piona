c45ca5823fcbc1082e41200bd47410125a711802
dima-antonenko <d.antonenko94@mail.ru>
2015-05-07 04:13:37 +0300

    add view to

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ActiveRecord::Base
 
 	before_destroy :ensure_not_referenced_by_any_line_item
 
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "220x180" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "303x437>", :thumb => "220x180" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	private
